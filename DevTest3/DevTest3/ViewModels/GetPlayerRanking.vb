b7270507e83736eab907c50c0e8ec10ecd5fad5e
kpshubert <kpshubert@kpshubert.com>
2021-05-18 10:24:16 -0500

    Add required validation rule to name (if it's empty, it doesn't pass validation)

--- DevTest3/DevTest3/ViewModels/GetPlayerRanking.vb
+++ DevTest3/DevTest3/ViewModels/GetPlayerRanking.vb
@@ -1,6 +1,6 @@
-﻿
+﻿Imports System.ComponentModel.DataAnnotations
 Public Class GetPlayerRanking
-
+    <Required(ErrorMessage:="Name Required")>
     Property Name As String
     Property Ranking As Integer
 
